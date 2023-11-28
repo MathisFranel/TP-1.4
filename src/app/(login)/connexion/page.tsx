13f5ef6e0a3f4d01ad960daebf19ed6f8cf4c5ac
MathisFranel <99545915+MathisFranel@users.noreply.github.com>
2023-11-28 17:29:50 +0100

    WIP

--- src/app/(login)/connexion/page.tsx
+++ src/app/(login)/connexion/page.tsx
@@ -3,15 +3,27 @@ import {TextInput, Group, PasswordInput} from '@mantine/core';
 import { useForm ,zodResolver} from '@mantine/form';
 import z from 'zod';
 import Link from 'next/link'
-import { Button } from "tp-kit/components";
+import {Button, NoticeMessage, useZodI18n} from "tp-kit/components";
+import {createClientComponentClient} from "@supabase/auth-helpers-nextjs";
+import {useRouter} from "next/navigation";
+import {useState} from "react";
 
 
 export default function page(){
 
+    type FormValues = z.infer<typeof schema>;
+
+    const router = useRouter()
+    const supabase = createClientComponentClient()
     const schema = z.object({
-        email: z.string().email({ message: 'Invalid email' }),
-        password: z.string().min(6,"password must contain at least 6 carateres")
+        email: z.string().email(),
+        password: z.string().min(6)
     });
+    const [created, setCreated] = useState(false);
+    const [isValid, setIsValid] = useState(false);
+    const [message, setMessage] = useState("");
+
+    useZodI18n(z);
 
     const form = useForm(
         {
@@ -24,10 +36,33 @@ export default function page(){
 
 
     });
+    const handleSubmit = async (values: FormValues) => {
+        console.log(values);
+        const {error} = await supabase.auth.signUp(
+            {
+                email: values.email,
+                password: values.password,
+                options: {
+                    emailRedirectTo: 'http://localhost:3000/api/auth/callback',
+                    data: {}
+                }
+            }
+        )
+    }
 
-    return <>
+
+        return <>
+
             <p>CONNEXION</p>
-            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            {
+                created &&
+                <NoticeMessage
+                    className="w-full"
+                    type={isValid ? "success" : "error"}
+                    message={message}
+                />
+            }
+            <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
                 <TextInput
                     withAsterisk
                     label="Email"
--- src/app/(login)/inscription/page.tsx
+++ src/app/(login)/inscription/page.tsx
@@ -4,18 +4,48 @@ import { useForm ,zodResolver} from '@mantine/form';
 import z from 'zod';
 import Link from 'next/link'
 import {Button, ZodI18nProvider, NoticeMessage, useZodI18n} from "tp-kit/components";
-
+import {useRouter} from "next/navigation";
+import {createClientComponentClient} from "@supabase/auth-helpers-nextjs";
+import {useState} from "react";
 
 export default function page(){
+
+    type FormValues = z.infer<typeof schema>;
+
+    const router = useRouter()
+    const supabase = createClientComponentClient()
 
     const schema = z.object({
         name : z.string().nonempty(),
         email: z.string().email(),
         password: z.string().min(6,)
     });
+    const handleSubmit = async (values: FormValues) => {
+        console.log(values);
+        const { error } = await supabase.auth.signUp(
+            {
+                email: values.email,
+                password: values.password,
+                options: {
+                    emailRedirectTo: 'http://localhost:3000/api/auth/callback',
+                    data: {
+                        name: values.name
+                    }
+                }
+            }
+        )
+
+        console.log(error)
+        setCreated(true);
+        setMessage((error) ? error.message : "Votre inscription a bien été prise en compte. Validez votre adresse email pour vous connecter")
+        setIsValid((!error))
+    }
+        const [created, setCreated] = useState(false);
+        const [isValid, setIsValid] = useState(false);
+        const [message, setMessage] = useState("");
 
 
-    useZodI18n(z);
+        useZodI18n(z);
     const form = useForm(
         {
             validate: zodResolver(schema),
@@ -31,8 +61,15 @@ export default function page(){
 
     return <>
         <p>INSCRIPTION</p>
-        <form onSubmit={form.onSubmit((values) => console.log(values))}>
-            <NoticeMessage message={"Erreur"}></NoticeMessage>
+        <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+            {
+                created &&
+                <NoticeMessage
+                    className="w-full"
+                    type={isValid ? "success" : "error"}
+                    message={message}
+                />
+            }
             <TextInput
                 withAsterisk
                 description={"Le nom qui sera utilisé pour vos commandes"}
@@ -61,7 +98,7 @@ export default function page(){
                 >
                     Connexion
                 </Button>
-                <Link href={"/connexion"}>Déjà un compte ? Se connecter</Link>
+                <a onClick={() => router.push('/connexion')} className="">Déjà un compte ? Se connecter</a>
             </Group>
 
         </form>
